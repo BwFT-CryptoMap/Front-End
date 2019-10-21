d4c12b5534666b6624a4b3106b79bfe8ead1e415
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-21 11:07:52 -0700

    added CoinList to App, added material UI as a dependency

--- cryptomaps/src/App.js
+++ cryptomaps/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {CoinList} from './components'
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <CoinList/>
+    </div>
   );
 }
 
