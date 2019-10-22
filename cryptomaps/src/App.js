3a6c91da598aada63243d121434bc9991a4de2ea
Austin Powell <apst12012@gmail.com>
2019-10-22 19:54:13 -0400

    conditional fill colors based on 24hour change, scroll feature, centered text based on rec size, scaled font size

--- cryptomaps/src/App.js
+++ cryptomaps/src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import {CoinList} from './components'
 import Blocklist from './components/TreeMap'
+import TreeMap from './components/TreeMap';
 
 function App() {
   return (
     <div>
-    <CoinList/>
+    {/* <CoinList/> */}
+    <TreeMap/>
     </div>
   );
 }
