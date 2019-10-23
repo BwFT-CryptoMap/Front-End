637cc9906b7fcd1b0405ff64a0bf9e08d72f1be3
Christopher Bailey <seabayley@outlook.com>
2019-10-23 10:03:14 -0500

    Added responsive sizing

--- cryptomaps/src/components/TreeMap.jsx
+++ cryptomaps/src/components/TreeMap.jsx
@@ -34,58 +34,58 @@ export default () => {
             {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
                 <React.Fragment>
                     <div className="tools">
-              <button onClick={resetTransform}>Reset</button>
+                        <button onClick={resetTransform}>Reset</button>
                     </div>
-            <TransformComponent>
-        <Svg width={1024} height={600}>
-            <Treemap
-                data={{
-                    children: arrangeData(mapData)
-                }}
-                sum={datum => datum.value}
-                size={[1024, 600]}
-                nodeEnter={d => ({ ...d, r: 0 })} 
-                animate
-                >
-                {nodes => nodes.map(({ key, x0, y0, x1, y1, data }) => (
-                    <>
-                <Rect
-                    key={key}
-                    x={x0}
-                    y={y0}
-                    width={x1 - x0}
-                    height={y1 - y0}
-                    fill={data.percentageChange24HrUsd === 0 ? '#BCB2B1' : data.percentageChange24HrUsd > 0 ? data.percentageChange24HrUsd >= 5 ? '#518651' : '#7EC17E' : data.percentageChange24HrUsd <= -5 ? '#C84040': '#ED7171'}
-                    stroke='black'
-                />
-            <Text 
-                x={x0 + (x1 - x0) / 2}
-                y={y0 + (y1 - y0) / 2}
-                fontSize={Number((x1-x0) /10)}
-                color="black"
-                > 
-              {data.symbol}
-            </Text>
-            <Text
-                   x={x0 + (x1-x0) / 2}
-                   y={y0 + (y1-y0) / 1.6}
-                   fontSize={Number((x1-x0) /12)}
-                   color="black">
-                       {data.priceUsd ? '$' + data.priceUsd.toFixed(2): null}
-                   </Text>
-                   <Text
-                   x={x0 + (x1-x0) / 2}
-                   y={y0 + (y1-y0) / 1.4}
-                   fontSize={Number((x1-x0) /14)}
-                   color="black">
-                       {data.percentageChange24HrUsd ? data.percentageChange24HrUsd > 0 ? '+' + data.percentageChange24HrUsd.toFixed(2) + '%' : data.percentageChange24HrUsd.toFixed(2) + '%' : null}
-                   </Text>
-                </>
-            ))}
-            </Treemap>
-        </Svg >
-        </TransformComponent>
-        </React.Fragment>
+                    <TransformComponent>
+                        <Svg width={window.innerWidth} height={window.innerHeight - 100}>
+                            <Treemap
+                                data={{
+                                    children: arrangeData(mapData)
+                                }}
+                                sum={datum => datum.value}
+                                size={[window.innerWidth, (window.innerHeight - 100)]}
+                                nodeEnter={d => ({ ...d, r: 0 })}
+                                animate
+                            >
+                                {nodes => nodes.map(({ key, x0, y0, x1, y1, data }) => (
+                                    <>
+                                        <Rect
+                                            key={key}
+                                            x={x0}
+                                            y={y0}
+                                            width={x1 - x0}
+                                            height={y1 - y0}
+                                            fill={data.percentageChange24HrUsd === 0 ? '#BCB2B1' : data.percentageChange24HrUsd > 0 ? data.percentageChange24HrUsd >= 5 ? '#518651' : '#7EC17E' : data.percentageChange24HrUsd <= -5 ? '#C84040' : '#ED7171'}
+                                            stroke='black'
+                                        />
+                                        <Text
+                                            x={x0 + (x1 - x0) / 2}
+                                            y={y0 + (y1 - y0) / 2}
+                                            fontSize={Number((x1 - x0) / 10)}
+                                            color="black"
+                                        >
+                                            {data.symbol}
+                                        </Text>
+                                        <Text
+                                            x={x0 + (x1 - x0) / 2}
+                                            y={y0 + (y1 - y0) / 1.6}
+                                            fontSize={Number((x1 - x0) / 12)}
+                                            color="black">
+                                            {data.priceUsd ? '$' + data.priceUsd.toFixed(2) : null}
+                                        </Text>
+                                        <Text
+                                            x={x0 + (x1 - x0) / 2}
+                                            y={y0 + (y1 - y0) / 1.4}
+                                            fontSize={Number((x1 - x0) / 14)}
+                                            color="black">
+                                            {data.percentageChange24HrUsd ? data.percentageChange24HrUsd > 0 ? '+' + data.percentageChange24HrUsd.toFixed(2) + '%' : data.percentageChange24HrUsd.toFixed(2) + '%' : null}
+                                        </Text>
+                                    </>
+                                ))}
+                            </Treemap>
+                        </Svg >
+                    </TransformComponent>
+                </React.Fragment>
             )}
         </TransformWrapper>
     )
